8a74c3a73deb91dc7be5382f7b0a4f74338591ff
IamSituma <amb.situma@gmail.com>
2026-02-10 16:47:43 +0300

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,26 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-}
 
+  webpack: (config) => {
+    // Set @ to point to project root
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
